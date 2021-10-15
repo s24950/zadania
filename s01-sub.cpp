37531a8fa15760b2007ac1a736ae6c58aeacfe0b
s24950 <91725465+s24950@users.noreply.github.com>
2021-10-15 11:13:55 +0200

    Update s01-sub.cpp

--- s01-sub.cpp
+++ s01-sub.cpp
@@ -1,12 +1,3 @@
-include <iostream>
-# include <string>
-// for std :: cout , std :: cerr , and std :: cin
-auto main ( int argc , char * argv []) -> int
-{
-auto const a =std:: stoi ( argv [1]);
-auto const b =std:: stoi ( argv [2]);
-std :: cout << ( a - b ) << "\n";
-return 0;
 #include <iostream>
 # include <string>
 // for std :: cout , std :: cerr , and std :: cin
@@ -16,5 +7,4 @@ auto const a =std:: stoi ( argv [1]);
 auto const b =std:: stoi ( argv [2]);
 std :: cout << ( a - b ) << "\n";
 return 0;
-}
 }
