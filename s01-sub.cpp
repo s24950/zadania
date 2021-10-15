1e1ed7e46a86ef697160d2fe65d665f891f68bbd
Student <s24950@pjwstk.edu.plgit config --global user.email s24950@pjwstk.edu.pl>
2021-10-15 11:21:58 +0200

    02 kalkulator odejmowanie

--- s01-sub.cpp
+++ s01-sub.cpp
@@ -2,6 +2,7 @@
 # include <string>
 // for std :: cout , std :: cerr , and std :: cin
 auto main ( int argc , char * argv []) -> int
+
 {
 auto const a =std:: stoi ( argv [1]);
 auto const b =std:: stoi ( argv [2]);
