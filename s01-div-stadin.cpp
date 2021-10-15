2442903fe834ff4215adc0e72559be70be5c8898
Student <s24950@pjwstk.edu.plgit config --global user.email s24950@pjwstk.edu.pl>
2021-10-15 12:01:03 +0200

    s01-div-stadin.cpp

---
+++ s01-div-stadin.cpp
@@ -0,0 +1,15 @@
+# include < iostream >
+# include < string >
+auto main () -> int
+{
+auto tmp = std :: string {};
+// temporary variable
+std :: getline ( std :: cin , tmp );
+std :: cout << " a = ";
+auto const a = std :: stoi ( tmp );
+std :: cout << " b = ";
+std :: getline ( std :: cin , tmp );
+auto const b = std :: stoi ( tmp );
+std :: cout << ( a / b ) << "\ n ";
+return 0;
+}
